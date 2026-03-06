a4158e5cd149282013f260cdb7de406a47243c9c
Wizard77-exe <jhayedc0@gmail.com>
2026-03-06 21:56:12 +0800

    + Admin & NormalUser

--- file_permission_simulator/file-permission-simulator.cpp
+++ file_permission_simulator/file-permission-simulator.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+using namespace std;
+
+
 const unsigned int READ = 1;
 const unsigned int WRITE = 1 << 1;      // 2
 const unsigned int DELETE = 1 << 2;     // 4
@@ -10,17 +13,37 @@ const unsigned int ROOT = 0 | READ | WRITE | DELETE | EXECUTE;       // 16
 class User {
     public: 
         unsigned int permission = 0;    // initially 0
+        string username;
+        string password;
 
         bool can(unsigned int p) { return (permission & p);}
 };
 
-int main() {
-    User a;
-    a.permission |= READ;
-    a.permission |= WRITE;
-    a.permission |= DELETE;
+class NormalUser : public User {
+    public: 
+};
 
-    std::cout << "User a can read? " << a.can(EXECUTE) << "\n";
+class Admin : public User {
+    public: 
+        void setPermission(NormalUser *x, const unsigned int p) {
+            x->permission |= p;
+        }
+        void togglePermission(NormalUser *x, const unsigned int p) {
+            x->permission ^= p;
+        }
+        void revokePermission(NormalUser *x, const unsigned int p) {
+            x->permission &= ~p;
+        }
+};
+
+int main() {
+    NormalUser a;
+    Admin b;
+
+    b.setPermission(&a, READ);
+
+    cout << "User a can read? " << a.can(READ) << "\n";
+    cout << "User a can write? " << a.can(WRITE) << "\n";
 
     return 0;
 }
