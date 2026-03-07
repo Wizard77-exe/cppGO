a5e7073c18cbf8bff8f72f79bf272f170dcd0181
Wizard77-exe <jhayedc0@gmail.com>
2026-03-07 23:46:51 +0800

    finished fpSim v1

--- file_permission_simulator/file-permission-simulator.cpp
+++ file_permission_simulator/file-permission-simulator.cpp
@@ -9,7 +9,7 @@ const unsigned int WRITE = 1 << 1;   // 2
 const unsigned int DELETE = 1 << 2;  // 4
 const unsigned int EXECUTE = 1 << 3; // 8
 
-const unsigned int ROOT = 0 | READ | WRITE | DELETE | EXECUTE; // 16
+const unsigned int ROOT = 0 | READ | WRITE | DELETE | EXECUTE; // 15
 
 const string RED = "\x1b[31m";
 const string GREEN = "\x1b[32m";
@@ -26,7 +26,7 @@ public:
     string username;
     unsigned int id;
 
-    bool can(unsigned int p) { return (permission & p); }
+    bool can(unsigned int p) { return (permission & p) == p; }
 
     void setPassword(string psswd)
     {
@@ -89,18 +89,105 @@ public:
 class Admin : public User
 {
 public:
-    void setPermission(NormalUser *x, const unsigned int p)
-    {
+    void setPermission(NormalUser *x)
+    {   
+        int choice;
+        unsigned int p;
+        cout << "==============================\n";
+        cout << "[1] WRITE\n";
+        cout << "[2] READ\n";
+        cout << "[3] DELETE\n";
+        cout << "[4] EXECUTE\n";
+        cout << "[5] ROOT\n";
+        cout << "[0] Exit\n";
+        cout << "==============================\n";
+        while(!(cin >> choice)) {
+            cout << "Enter Permission to Set: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        if (choice == 0) return;
+        switch (choice) {
+            case 1: p = WRITE; break;
+            case 2: p = READ; break;
+            case 3: p = DELETE; break;
+            case 4: p = EXECUTE; break;
+            case 5: p = ROOT; break;
+        }
         x->permission |= p;
+        if (x->can(p)) {
+            cout << "Setting Permission Successful\n";
+        } else {
+            cout << "Setting Permission Error!\n";
+        }
     }
-    void togglePermission(NormalUser *x, const unsigned int p)
+    void togglePermission(NormalUser *x)
     {
+        int choice;
+        unsigned int p;
+        cout << "==============================\n";
+        cout << "[1] WRITE\n";
+        cout << "[2] READ\n";
+        cout << "[3] DELETE\n";
+        cout << "[4] EXECUTE\n";
+        cout << "[5] ROOT\n";
+        cout << "[0] Exit\n";
+        cout << "==============================\n";
+        while(!(cin >> choice)) {
+            cout << "Enter Permission to TOGGLE: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        if (choice == 0) return;
+        switch (choice) {
+            case 1: p = WRITE; break;
+            case 2: p = READ; break;
+            case 3: p = DELETE; break;
+            case 4: p = EXECUTE; break;
+            case 5: p = ROOT; break;
+        }
+        int currP = x->can(p);
+
         x->permission ^= p;
+        if (x->can(p) != currP) {
+            cout << "Setting Permission Successful\n";
+        } else {
+            cout << "Setting Permission Error!\n";
+        }
     }
-    void revokePermission(NormalUser *x, const unsigned int p)
+    void revokePermission(NormalUser *x)
     {
+        int choice;
+        unsigned int p;
+        cout << "==============================\n";
+        cout << "[1] WRITE\n";
+        cout << "[2] READ\n";
+        cout << "[3] DELETE\n";
+        cout << "[4] EXECUTE\n";
+        cout << "[5] ROOT\n";
+        cout << "[0] Exit\n";
+        cout << "==============================\n";
+        while(!(cin >> choice)) {
+            cout << "Enter Permission to REVOKE: ";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        if (choice == 0) return;
+        switch (choice) {
+            case 1: p = WRITE; break;
+            case 2: p = READ; break;
+            case 3: p = DELETE; break;
+            case 4: p = EXECUTE; break;
+            case 5: p = ROOT; break;
+        }
         x->permission &= ~p;
+        if (!(x->can(p))) {
+            cout << "Revoking Permission Successful\n";
+        } else {
+            cout << "Revoking Permission Error!\n";
+        }
     }
+    void createAccount();
 };
 
 vector<Admin> adminList;
@@ -240,16 +327,28 @@ int normalUserLogin()
     {
         return 0;
     }
-    cout << BLUE << "Login Successful!\n"
-         << RESET;
+    cout << BLUE << "Login Successful!\n" << RESET;
     return uid;
+}
+
+void Admin::createAccount() {
+    string role;
+    do {
+        cout << "Enter Role for New User: ";
+        getline(cin, role);
+    } while (role.empty());
+    if (role == "normal user") {
+        addNormalUser(&normalUserList);
+    } else {
+        addAdmin(&adminList);
+    }
 }
 
 int main()
 {
     string name, password, currUserRole;
     unsigned int uid;
-    int index;
+    int index, choice;
 
     if (adminList.size() == 0 && normalUserList.size() == 0)
     {
@@ -265,8 +364,7 @@ int main()
 
     while (1)
     {
-        cout << BLUE << "\nWelcome to File System Simulator\n"
-             << RESET;
+        cout << BLUE << "\nWelcome to File System Simulator\n" << RESET;
         cout << "==================================================\n";
         cout << RED << "[Just Click Enter to Exit]\n";
         cout << "[Enter \'admin\' to go admin]\n";
@@ -286,9 +384,40 @@ int main()
             else
             {
                 // successful login
-                cout << "Login Successful" << endl;
-                cout << adminList[uid - 1].username << " " << adminList[uid - 1].id << endl;
-                break;
+                Admin user = adminList[uid - 1];
+                
+                cout << BLUE << "Login Successful\n\n" << RESET;
+                cout << "==================================================\n";
+                cout << "Welcome " << user.username << endl;
+                cout << "==================================================\n";
+
+                while(1) {
+                    cout << "[1] CREATE New Account\n";
+                    cout << "[2] SET Permission\n";
+                    cout << "[3] REVOKE Permission\n";
+                    cout << "[4] TOGGLE Permission\n";
+                    cout << "[5] LOGOUT\n";
+                    cout << "[0] EXIT";
+                    cout << "Enter your Choice: ";
+
+                    while(!(cin >> choice)) {
+                        cout << "Invalid Choice!\n\n";
+                        cin.clear();
+                        cin.ignore(10000, '\n');
+                    }
+                    if (choice == 5) {
+                        break;
+                    }
+                    /*switch(choice) {
+                        case 1: user.createAccount(); break;
+                        case 2: user.setPermission(); break;
+                        case 3: user.revokePermission(); break;
+                        case 4: user.togglePermission(); break;
+                        case 0: exit(0);
+                        default: cout << RED << "Invalid Input!\n\n" << RESET;
+                    }*/
+                }
+
             }
         }
         else if (currUserRole == "normal user")
@@ -302,10 +431,38 @@ int main()
             }
             else
             {
+                NormalUser user = normalUserList[uid - 1];
                 // successful login
-                cout << "Login Successful" << endl;
-                cout << normalUserList[uid - 1].username << " " << normalUserList[uid - 1].id << endl;
-                break;
+                cout << BLUE << "Login Successful\n\n" << RESET;
+                cout << "==================================================\n";
+                cout << "Welcome " << user.username << endl;
+                cout << "==================================================\n";
+                while(1) {
+                    cout << "[1] READ file\n";
+                    cout << "[2] WRITE file\n";
+                    cout << "[3] DELETE file\n";
+                    cout << "[4] EXECUTE file\n";
+                    cout << "[5] LOGOUT\n";
+                    cout << "[0] EXIT";
+                    cout << "Enter your Choice: ";
+
+                    while(!(cin >> choice)) {
+                        cout << "Invalid Choice!\n\n";
+                        cin.clear();
+                        cin.ignore(10000, '\n');
+                    }
+                    if (choice == 5) {
+                        break;
+                    }
+                    switch(choice) {
+                        case 1: user.action(READ); break;
+                        case 2: user.action(WRITE); break;
+                        case 3: user.action(DELETE); break;
+                        case 4: user.action(EXECUTE); break;
+                        case 0: exit(0);
+                        default: cout << RED << "Invalid Input!\n\n" << RESET;
+                    }
+                }
             }
         }
         else
